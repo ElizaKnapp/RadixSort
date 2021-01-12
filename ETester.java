ff77066fe539206f4efde7be4922c0d5f469ceb9
ElizaKnapp <eknapp20@stuy.edu>
2021-01-12 11:54:00 -0500

    fixed bug for negative numbers

---
+++ ETester.java
@@ -0,0 +1,6 @@
+public class ETester {
+  public static void main(String[] args) {
+    int n = Radix.nth(-123, 2);
+    System.out.println(n);
+  }
+}
--- Radix.java
+++ Radix.java
@@ -1,6 +1,7 @@
 public class Radix{
   public static int nth(int n, int col) {
-    int div = n / Math.pow(10, col);
+    if (n < 0) n = n * -1;
+    int div = n / (int)Math.pow(10, col);
     return div % 10;
   }
 
