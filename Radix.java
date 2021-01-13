1dce241416dc73308c417b39a63bafec37a04a4c
ElizaKnapp <eknapp20@stuy.edu>
2021-01-13 16:29:18 -0500

    simple sort done

--- Radix.java
+++ Radix.java
@@ -13,19 +13,19 @@ public class Radix{
 
   public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
     for (int i = 0; i < buckets.length; i++) {
-      original.extend(i);
+      original.extend(buckets[i]);
     }
   }
 
   public static void radixSortSimple(SortableLinkedList data) {
-    SortableLinkedList[] buckets = new SortableLinkedList();
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
     for (int i = 0; i < 10; i ++) {
       buckets[i] = new SortableLinkedList();
     }
 
     //least significant pass- also here calc the largest number
     int maxPlace = 0;
-    while (data != 0) {
+    while (data.size() != 0) {
       int holder = data.get(0);
       if (length(holder) > maxPlace) maxPlace = length(holder);
       buckets[nth(holder, 0)].add(holder);
@@ -35,8 +35,22 @@ public class Radix{
     merge(data, buckets);
 
     for (int i = 0; i < maxPlace; i++) {
-      
+      //empty the buckets
+      buckets = new SortableLinkedList[10];
+      for (int j = 0; j < 10; j++) {
+        buckets[i] = new SortableLinkedList();
+      }
+      //add the terms by place value of i
+      while (data.size() != 0) {
+        int holder = data.get(0);
+        buckets[nth(holder, i)].add(holder);
+        data.remove(0);
+      }
+      merge(data, buckets);
     }
+
   }
+
+
 
 }
